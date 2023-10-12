286b96c94e3112981fe66ea8a57db58de9576789
joshshih <143784173+joshshihtw@users.noreply.github.com>
2023-10-12 17:29:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,4 @@ import logging
 
 logging.warning('Watch out!')  # will print a message to the console
 logging.info('I told you so')  # will not print anything
-logging.info('I tddddold you so')  # will not print anything
-logging.info('I tddddold you so')  # will not print anything
-logging.info('I tddddold you so')  # will not print anything
+logging.info('I tddddold you so')  # will not print anythin
